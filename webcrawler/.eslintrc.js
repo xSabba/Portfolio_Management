2ae81570038864b842caa486f04fcab57b46d8d9
Raphael Wolfsberger <wolfsberger.r03@htl-ottakring.ac.at>
2021-10-10 10:13:58 +0200

    web crawler initialization

---
+++ webcrawler/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'object-curly-newline': 0,
+    'no-console': 0,
+    'no-restricted-syntax': 0,
+    'no-plusplus': 0,
+    'implicit-arrow-linebreak': 0,
+    'function-paren-newline': 0,
+  },
+};
