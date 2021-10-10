b1fbbf0983f5479cdf6f9c9f8f2bba6e39243544
Raphael Wolfsberger <wolfsberger.r03@htl-ottakring.ac.at>
2021-10-10 10:29:33 +0200

    web crawler init v2

---
+++ webcrawler/index.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => console.log('running'));
+
